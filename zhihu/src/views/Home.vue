4ea53103295883fd61c8d3c51c745449bb95d8b4
LehaoLin <lehaolin@link.cuhk.edu.cn>
2020-07-06 14:20:09 +0800

    ADD: search bar; tab bar; refresh

--- zhihu/src/views/Home.vue
+++ zhihu/src/views/Home.vue
@@ -1,17 +1,106 @@
 <template>
-  <div class="home">
+  <div>
+
+    <div style="position:sticky;top:0px;z-index:999;">
+        <van-search
+          v-model="value"
+          show-action
+          placeholder="请输入搜索关键词"
+          @search="onSearch"
+          >
+          <template #action>        
+            <van-icon name="add-o" @click="onAdd"></van-icon>
+          </template>
+        </van-search>
+    </div>
+
+    
+      <van-pull-refresh v-model="isLoading" @refresh="onRefresh" style="height:99999vw;">
+        <p>刷新次数: {{ count }}</p>
+       </van-pull-refresh>
+
+
+       <van-list
+        v-model="loading"
+        :finished="finished"
+        finished-text="没有更多了"
+        @load="onLoad"
+      >
+        <van-cell v-for="item in list" :key="item" :title="item" />
+      </van-list>
+    
+      <van-tabbar v-model="active" @change="onChange">
+        <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+        <van-tabbar-item icon="like-o">收藏</van-tabbar-item>
+        <van-tabbar-item icon="friends-o">朋友</van-tabbar-item>
+        <van-tabbar-item icon="user-o">个人</van-tabbar-item>
+      </van-tabbar>
+     
     
   </div>
 </template>
 
 <script>
+
+import { Notify } from 'vant';
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
   components: {
-    
+  },
+  data(){
+    return {
+      list:[],
+      count: 0,
+      value:'',
+      active:'',
+      isLoading: false,
+      loading: false,
+      finished: false,
+    }
+  },
+  methods:{
+    onChange(index) {
+      Notify({ type: 'primary', message: index });
+    },    
+    onSearch(val){
+      Notify({ type: 'primary', message: val });
+      console.log("val", val);
+    },
+    onAdd(){
+      console.log("添加")
+    },
+    onRefresh() {
+      setTimeout(() => {
+        this.isLoading = false;
+        this.count++;
+      }, 1000);
+    },
+    onLoad() {
+      // 异步更新数据
+      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1);
+        }
+
+        // 加载状态结束
+        this.loading = false;
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          this.finished = true;
+        }
+      }, 1000);
+    },
+
   }
 }
 </script>
+
+<style>
+# search {
+  margin: 0px;
+}
+</style>
